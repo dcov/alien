26cb6c9054d41490012876d1ee8c073a69dd2321
Diego Covarrubias <dcov@pm.me>
2020-02-18 14:05:59 -0800

    Refactor Snudown module into separate files

--- app/lib/snudown/snudown_model.dart
+++ app/lib/snudown/snudown_model.dart
@@ -1,4 +1,9 @@
-part of 'snudown.dart';
+import 'package:elmer/elmer.dart';
+import 'package:markdown/markdown.dart';
+
+import 'snudown_parser.dart' as parser;
+
+part 'snudown_model.g.dart';
 
 abstract class Snudown implements Model {
 
@@ -7,7 +12,7 @@ abstract class Snudown implements Model {
       nodes: const <Node>[],
       models: const <String, Model>{}
     );
-    parseSnudown(result, data);
+    parser.parseRawInto(data, result);
     return result;
   }
 
