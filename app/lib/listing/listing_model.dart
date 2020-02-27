6bdb42fce50a21dffbda976ac4f9a960ff6c30f5
Diego Covarrubias <dcov@pm.me>
2020-02-27 13:59:25 -0800

    Make Listing model instantiable, Add bounded generic argument to it, Add factory constructor

--- app/lib/listing/listing_model.dart
+++ app/lib/listing/listing_model.dart
@@ -1,8 +1,11 @@
 import 'package:elmer/elmer.dart';
+import 'package:meta/meta.dart';
 import 'package:reddit/reddit.dart';
 
 import '../thing/thing_model.dart';
 import '../widgets/scroll_offset.dart';
+
+part 'listing_model.g.dart';
 
 enum ListingMode {
   single,
@@ -16,14 +19,32 @@ enum ListingStatus {
   loadingPrevious
 }
 
-@abs
-abstract class Listing implements Model {
+abstract class Listing<T extends Thing> implements Model {
+
+  factory Listing({
+    @required ListingMode mode,
+    ListingStatus status = ListingStatus.idle,
+    @required List<T> things,
+    Pagination pagination,
+    @required ScrollOffset offset,
+  }) {
+    assert(mode != null);
+    assert(things != null);
+    assert(offset != null);
+
+    return _$Listing<T>(
+      mode: mode,
+      status: status,
+      things: things,
+      pagination: pagination,
+      offset: offset);
+  }
 
   ListingMode mode;
 
   ListingStatus status;
 
-  List<Thing> get things;
+  List<T> get things;
 
   Pagination pagination;
 
