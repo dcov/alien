7d693ff1610340861d82d60b2c91bb2201dab98e
Diego Covarrubias <dcov@pm.me>
2020-03-03 21:49:43 -0800

    Add HomeTile widget

--- app/lib/home/home_widgets.dart
+++ app/lib/home/home_widgets.dart
@@ -1,13 +1,34 @@
 import 'package:elmer_flutter/elmer_flutter.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 import '../listing/listing_model.dart' show ListingStatus;
 import '../listing/listing_widgets.dart';
 import '../post/post_model.dart';
 import '../post/post_widgets.dart';
+import '../widgets/widget_extensions.dart';
 
 import 'home_events.dart';
 import 'home_model.dart';
+
+class HomeTile extends StatelessWidget {
+
+  HomeTile({
+    Key key,
+    @required this.home,
+  }) : super(key: key);
+
+  final Home home;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () => context.push(HomePage.createRoute(home)),
+      leading: Icon(Icons.home),
+      title: Text('Home'),
+    );
+  }
+}
 
 class HomePage extends StatelessWidget {
 
