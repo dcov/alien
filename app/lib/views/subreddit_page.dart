6b182ab0f28468aac4a18f8b7724967ce28d8d98
Diego Covarrubias <dcov@pm.me>
2020-08-24 10:22:47 -0700

    Move subreddit_tile widgets to subreddit_page to better handle transitions

--- app/lib/views/subreddit_page.dart
+++ app/lib/views/subreddit_page.dart
@@ -3,7 +3,53 @@ import 'package:flutter/material.dart';
 
 import '../logic/thing.dart';
 import '../models/subreddit.dart';
+import '../widgets/icons.dart';
+import '../widgets/tile.dart';
 import '../widgets/routing.dart';
+
+class SubredditTile extends StatelessWidget {
+
+  SubredditTile({
+    Key key,
+    @required this.subreddit,
+    this.includeDepth = false,
+    this.pageNamePrefix = ''
+  }) : super(key: key);
+
+  final Subreddit subreddit;
+
+  final bool includeDepth;
+
+  final String pageNamePrefix;
+
+  void _pushPage(BuildContext context) {
+    context.push(
+      SubredditPage.pageNameFrom(subreddit, pageNamePrefix),
+      (String pageName) {
+        return SubredditPage(
+          subreddit: subreddit,
+          name: pageName);
+      });
+  }
+
+  @override
+  Widget build(_) => Connector(
+    builder: (BuildContext context) {
+      return CustomTile(
+        onTap: () => _pushPage(context),
+        icon: Icon(
+          CustomIcons.subreddit,
+          color: Colors.blueGrey,
+        ),
+        title: Text(
+          subreddit.name,
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+        ),
+      );
+    },
+  );
+}
 
 class SubredditPage extends EntryPage {
 
