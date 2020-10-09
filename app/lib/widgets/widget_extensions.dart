3bb446520066c3e02ca65a0db1d6326522168a87
Diego Covarrubias <dcov@pm.me>
2020-10-08 18:34:33 -0700

    Merge MediaQueryExtensions and ThemeExtensions into single WidgetExtensions extension block

--- app/lib/widgets/widget_extensions.dart
+++ app/lib/widgets/widget_extensions.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 
-extension MediaQueryExtensions on BuildContext {
+extension WigetExtensions on BuildContext {
 
   EdgeInsets get mediaPadding {
     return MediaQuery.of(this).padding;
   }
-}
-
-extension ThemeExtensions on BuildContext {
 
   ThemeData get theme => Theme.of(this);
 }
