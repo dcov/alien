15cd2c2f920cbd6a3c80588f5877e94edeb93b98
Diego Covarrubias <dcov@pm.me>
2021-02-25 17:27:44 -0800

    Finish implementing PathRouter and tests

--- app/lib/utils/path_router.dart
+++ app/lib/utils/path_router.dart
@@ -28,7 +28,7 @@ class PathNode<R extends PathRoute> {
 enum PathRouterGoToTransition {
   pushFromEmpty,
   push,
-  pop,
+  goBack,
   replace,
   none,
 }
@@ -46,6 +46,7 @@ class PathRouterGoToResult<R extends PathRoute> {
 }
 
 enum PathRouterRemoveTransition {
+  popToEmpty,
   pop,
   replace,
   none,
@@ -63,33 +64,36 @@ class PathRouterRemoveResult<R extends PathRoute> {
   final List<R> stack;
 }
 
-enum PathRouterMoveTransition {
-  replace,
-  none,
-}
+class PathRouterDetachResult<R extends PathRoute> {
 
-class PathRouterMoveResult<R extends PathRoute> {
-
-  PathRouterMoveResult._({
-    required this.transition,
+  PathRouterDetachResult._({
     required this.stack
   });
-
-  final PathRouterMoveTransition transition;
 
   final List<R> stack;
 }
 
 class PathRouter<R extends PathRoute> {
 
-  late final nodes = UnmodifiableMapView<String, PathNode<R>>(_nodes);
+  late final UnmodifiableMapView<String, PathNode<R>> nodes = UnmodifiableMapView<String, PathNode<R>>(_nodes);
   final _nodes = <String, PathNode<R>>{};
   var _stack = <String>[];
+
+  List<R> _buildRouteStack() {
+    final result = <R>[];
+    PathNode<R>? parentNode;
+    for (var i = 0; i < _stack.length; i++) {
+      final node = parentNode != null ? parentNode._children[_stack[i]]! : _nodes[_stack[i]]!;
+      result.add(node.route);
+      parentNode = node;
+    }
+    return result;
+  }
 
   PathRouterGoToResult<R> goTo(
       String path, {
       required PathRouteFactory<R> onCreateRoute,
-      required PathRouteUpdateCallback<R> onUpdateRoute,
+      PathRouteUpdateCallback<R>? onUpdateRoute,
     }) {
     final oldStack = _stack;
     final newStack = path.split("/");
@@ -111,7 +115,7 @@ class PathRouter<R extends PathRoute> {
       }
 
       final isLastFragment = (i == (newStack.length - 1));
-      final isInOldStack = (i < oldStack.length && oldStack[i] == fragment);
+      final isInOldStack = (i < oldStack.length && oldStack[i] == fragment && parentsAreInOldStack);
 
       if (node == null) {
         // This should be the last fragment in the path
@@ -129,35 +133,44 @@ class PathRouter<R extends PathRoute> {
           parentNode._children[fragment] = newNode;
         }
 
-        if (parentsAreInOldStack) {
-          if (oldStack.length == newStack.length - 1) {
-            // onPushRoute(newRoute);
+        if (oldStack.isEmpty) {
+          if (newStack.length == 1) {
+            transition = PathRouterGoToTransition.pushFromEmpty;
           } else {
-            // onReplaceStack(stack);
+            transition = PathRouterGoToTransition.replace;
+          }
+        } else if (parentsAreInOldStack) {
+          if (oldStack.length == newStack.length - 1) {
+            transition = PathRouterGoToTransition.push;
+          } else {
+            transition = PathRouterGoToTransition.replace;
           }
         } else {
-          // onReplaceStack(stack);
+          transition = PathRouterGoToTransition.replace;
         }
       } else if (isLastFragment) {
         assert(node.route.fragment == fragment);
         assert(node.route.path == path);
-        onUpdateRoute(node.route);
+        if (onUpdateRoute != null) {
+          onUpdateRoute(node.route);
+        }
         // Check if the new stack matches the new stack up to this point, in which case it might be
         // the exact same stack, a popped route (that isn't removed from the tree), or just a replace.
         if (parentsAreInOldStack && isInOldStack) {
           if (oldStack.length == newStack.length) {
             // The Stack did not change
-            // onSameStack();
+            transition =  PathRouterGoToTransition.none;
           } else if (newStack.length == oldStack.length - 1) {
-            // The stack was popped
-            // onPopRoute();
+            // The stack was popped, but since this was a call to goTo and not remove, the route
+            // is not removed and instead we just 'go back' to the parent.
+            transition = PathRouterGoToTransition.goBack;
           } else {
             // The stack was popped more than one route so we'll treat it as a replace
-            // onReplaceStack(stack);
+            transition = PathRouterGoToTransition.replace;
           }
         } else {
           // The parents aren't the same, or this route isn't the same so it's a replace.
-          // onReplaceStack(stack);
+          transition = PathRouterGoToTransition.replace;
         }
       } else {
         parentNode = node;
@@ -165,20 +178,133 @@ class PathRouter<R extends PathRoute> {
       }
     }
 
-    return PathRouterGoToResult._(transition: PathRouterGoToTransition.none, stack: <R>[]);
+    return PathRouterGoToResult._(
+      transition: transition,
+      stack: _buildRouteStack());
   }
 
-  void pop(
-      String path, {
-      required 
-    }) {
-    // TODO
+  PathRouterRemoveResult<R> remove(String path) {
+    final fragments = path.split("/");
+
+    late PathRouterRemoveTransition transition;
+
+    PathNode<R>? parentNode;
+    bool parentsAreInStack = true;
+    for (var i = 0; i < fragments.length; i++) {
+      final fragment = fragments[i];
+
+      final PathNode<R> node;
+      if (parentNode == null) {
+        node = _nodes[fragment]!;
+      } else {
+        node = parentNode._children[fragment]!;
+      }
+
+      final isLastFragment = (i == fragments.length - 1);
+      final isInStack = (i < _stack.length && _stack[i] == fragment && parentsAreInStack);
+
+      if (isLastFragment) {
+        assert(node.route.fragment == fragment);
+        assert(node.route.path == path);
+        // The node does not have a 
+        node.route.._fragment = null
+                  .._path = null;
+
+        if (i == 0 && isInStack) {
+          _nodes.remove(fragment);
+          _stack.clear();
+          transition =  PathRouterRemoveTransition.popToEmpty;
+        } else {
+          parentNode!._children.remove(fragment);
+          if (parentsAreInStack && isInStack) {
+            if (isInStack && _stack.length == fragments.length) {
+              _stack.removeLast();
+              transition = PathRouterRemoveTransition.pop;
+            } else {
+              _stack.removeRange(i, _stack.length);
+              transition = PathRouterRemoveTransition.replace;
+            }
+          } else {
+            // We removed a subtree that was not in the stack
+            transition = PathRouterRemoveTransition.none;
+          }
+        }
+      } else {
+        parentNode = node;
+        parentsAreInStack = (parentsAreInStack && isInStack);
+      }
+    }
+
+    return PathRouterRemoveResult._(
+      transition: transition,
+      stack: _buildRouteStack());
   }
 
-  void move(
-      String fromPath,
-      String toPath
-    ) {
-    // TODO
+  PathRouterDetachResult<R> detach(String path, String newFragment) {
+    final fragments = path.split('/');
+
+    PathNode<R>? parentNode;
+    bool parentsAreInStack = true;
+    for (var i = 0; i < fragments.length; i++) {
+      final fragment = fragments[i];
+
+      final PathNode<R> node;
+      if (parentNode == null) {
+        node = _nodes[fragment]!;
+      } else {
+        node = parentNode._children[fragment]!;
+      }
+
+      final isLastFragment = (i == fragments.length - 1);
+      final isInStack = (i < _stack.length && _stack[i] == fragment && parentsAreInStack);
+
+      if (isLastFragment) {
+        assert(node.route.fragment == fragment);
+        assert(node.route.path == path);
+
+        node.route.._fragment = newFragment;
+        node.route.._path = newFragment;
+
+        if (node.children.isNotEmpty) {
+          _updateChildrenPaths(node.children, (String oldPath) {
+            final fragments = oldPath.split('/');
+            final newFragments = fragments.sublist(i);
+            newFragments[0] = newFragment;
+            return newFragments.join('/');
+          });
+        }
+
+        if (parentNode == null) {
+          assert(_nodes[fragment] == node);
+          _nodes.remove(fragment);
+          _nodes[newFragment] = node;
+          if (isInStack) {
+            _stack[0] = newFragment;
+          }
+        } else {
+          assert(parentNode._children[fragment] == node);
+          parentNode._children.remove(fragment);
+          _nodes[newFragment] = node;
+          if (isInStack) {
+            _stack = _stack.sublist(i);
+            _stack[0] = newFragment;
+          }
+        }
+      } else {
+        parentNode = node;
+        parentsAreInStack = (parentsAreInStack && isInStack);
+      }
+    }
+
+    return PathRouterDetachResult._(
+      stack: _buildRouteStack());
+  }
+
+  void _updateChildrenPaths(Map<String, PathNode> children, String updatePath(String oldPath)) {
+    for (final child in children.values) {
+      child.route._path = updatePath(child.route.path);
+      if (child.children.isNotEmpty)
+        _updateChildrenPaths(child.children, updatePath);
+    }
   }
 }
--- app/test/utils/path_router_test.dart
+++ app/test/utils/path_router_test.dart
@@ -11,52 +11,151 @@ class _TestRoute extends PathRoute {
   String toString() => name;
 }
 
-void _voidUpdate(_) { }
-
-PathRouteFactory<_TestRoute> _factoryFor(String name) {
+PathRouteFactory<_TestRoute> _factory(String name) {
   return () => _TestRoute(name);
 }
 
-final PathRouteFactory<_TestRoute> _errorFactory = () {
+_TestRoute _errorFactory() {
   throw StateError('_errorFactory does not expect to be called.');
-};
+}
+
+String _fragmentsListString(List<_TestRoute> stack) {
+  return stack.map((route) => route.fragment).toList().toString();
+}
+
+String _nodesString(Map<String, PathNode<_TestRoute>> nodes) {
+  return nodes.map<String, String>((String key, PathNode<_TestRoute> value) {
+    return MapEntry<String, String>(key, _nodesString(value.children));
+  }).toString();
+}
 
 void testPathRouter() {
   group('PathRouter', () {
-    group('goTo', () {
+    test('goTo', () {
       final router = PathRouter<_TestRoute>();
-      test('pushFromEmpty', () {
-        final result = router.goTo(
-          'a:0',
-          onCreateRoute: _factoryFor('a:0'),
-          onUpdateRoute: _voidUpdate);
-        expect(result.transition, PathRouterGoToTransition.pushFromEmpty);
-        expect(result.stack.toString(), ['a:0'].toString());
-      });
-      test('push', () {
-        final result = router.goTo(
-          'a:0/a:1',
-          onCreateRoute: _factoryFor('a:1'),
-          onUpdateRoute: _voidUpdate);
-        expect(result.transition, PathRouterGoToTransition.push);
-        expect(result.stack.toString(), ['a:0', 'a:1'].toString());
-      });
-      test('pop', () {
-        final result = router.goTo(
-          'a:0',
-          onCreateRoute: _errorFactory,
-          onUpdateRoute: _voidUpdate);
-        expect(result.transition, PathRouterGoToTransition.pop);
-        expect(result.stack.toString(), ['a:0'].toString());
-      });
-      test('replace', () {
-        final result = router.goTo(
-          'b:0',
-          onCreateRoute: _factoryFor('b:0'),
-          onUpdateRoute: _voidUpdate);
-        expect(result.transition, PathRouterGoToTransition.replace);
-        expect(result.stack.toString(), ['b:0'].toString());
-      });
+      var result = router.goTo('a0', onCreateRoute: _factory('a0'));
+      expect(result.transition, PathRouterGoToTransition.pushFromEmpty);
+      expect(result.stack.toString(), ['a0'].toString());
+      expect(_nodesString(router.nodes), {'a0': {}}.toString());
+
+      result = router.goTo('a0/a1', onCreateRoute: _factory('a1'));
+      expect(result.transition, PathRouterGoToTransition.push);
+      expect(result.stack.toString(), ['a0', 'a1'].toString());
+      expect(_nodesString(router.nodes), {'a0': { 'a1': {}}}.toString());
+
+      result = router.goTo('a0', onCreateRoute: _errorFactory);
+      expect(result.transition, PathRouterGoToTransition.goBack);
+      expect(result.stack.toString(), ['a0'].toString());
+      expect(_nodesString(router.nodes), {'a0': { 'a1': {}}}.toString());
+
+      result = router.goTo('b0', onCreateRoute: _factory('b0'));
+      expect(result.transition, PathRouterGoToTransition.replace);
+      expect(result.stack.toString(), ['b0'].toString());
+      expect(_nodesString(router.nodes),
+          {'a0': { 'a1': {}},
+           'b0': {}}.toString());
+
+      result = router.goTo('b0/b1', onCreateRoute: _factory('b1'));
+      expect(result.transition, PathRouterGoToTransition.push);
+      expect(result.stack.toString(), ['b0', 'b1'].toString());
+      expect(_nodesString(router.nodes),
+          {'a0': { 'a1': {}},
+           'b0': { 'b1': {}}}.toString());
+
+      result = router.goTo('a0/a1', onCreateRoute: _errorFactory);
+      expect(result.transition, PathRouterGoToTransition.replace);
+      expect(result.stack.toString(), ['a0', 'a1'].toString());
+      expect(_nodesString(router.nodes),
+          {'a0': { 'a1': {}},
+           'b0': { 'b1': {}}}.toString());
+
+      result = router.goTo('a0/a1', onCreateRoute: _errorFactory);
+      expect(result.transition, PathRouterGoToTransition.none);
+      expect(result.stack.toString(), ['a0', 'a1'].toString());
+      expect(_nodesString(router.nodes),
+          {'a0': { 'a1': {}},
+           'b0': { 'b1': {}}}.toString());
+
+      expect(() => router.goTo('c0/c1', onCreateRoute: _errorFactory), throwsAssertionError);
+      expect(_nodesString(router.nodes),
+          {'a0': { 'a1': {}},
+           'b0': { 'b1': {}}}.toString());
+    });
+
+    test('remove', () {
+      final router = PathRouter<_TestRoute>();
+        // add the 'a0' route
+      router.goTo('a0', onCreateRoute: _factory('a0'));
+      expect(_nodesString(router.nodes), {'a0': {}}.toString());
+
+      var result = router.remove('a0');
+      expect(result.transition, PathRouterRemoveTransition.popToEmpty);
+      expect(result.stack.toString(), [].toString());
+      expect(_nodesString(router.nodes), {}.toString());
+
+      router.goTo('a0', onCreateRoute: _factory('a0'));
+      router.goTo('a0/a1', onCreateRoute: _factory('a1'));
+      router.goTo('a0/a1/a2', onCreateRoute: _factory('a2'));
+      expect(_nodesString(router.nodes), {'a0': { 'a1': { 'a2': {}}}}.toString());
+
+      result = router.remove('a0/a1/a2');
+      expect(result.transition, PathRouterRemoveTransition.pop);
+      expect(result.stack.toString(), ['a0', 'a1'].toString());
+      expect(_nodesString(router.nodes), {'a0': { 'a1': {}}}.toString());
+
+      router.goTo('a0/a1/a2', onCreateRoute: _factory('a2'));
+      expect(_nodesString(router.nodes), {'a0': { 'a1': { 'a2': {}}}}.toString());
+      
+      result = router.remove('a0/a1');
+      expect(result.transition, PathRouterRemoveTransition.replace);
+      expect(result.stack.toString(), ['a0'].toString());
+      expect(_nodesString(router.nodes), {'a0': {}}.toString());
+
+      router.goTo('a0/a1', onCreateRoute: _factory('a1'));
+      router.goTo('a0/b1', onCreateRoute: _factory('b1'));
+      expect(_nodesString(router.nodes), {'a0': { 'a1': {}, 'b1': {}}}.toString());
+
+      result = router.remove('a0/a1');
+      expect(result.transition, PathRouterRemoveTransition.none);
+      expect(result.stack.toString(), ['a0', 'b1'].toString());
+      expect(_nodesString(router.nodes), {'a0': { 'b1': {}}}.toString());
+    });
+
+    test('detach', () {
+      final router = PathRouter<_TestRoute>();
+
+      router.goTo('a0', onCreateRoute: _factory('a0'));
+      router.goTo('a0/a1', onCreateRoute: _factory('a1'));
+      expect(_nodesString(router.nodes), {'a0': { 'a1': {}}}.toString());
+
+      var result = router.detach('a0/a1', 'b0');
+      expect(_fragmentsListString(result.stack), ['b0'].toString());
+      expect(_nodesString(router.nodes), {'a0': {}, 'b0': {}}.toString());
+
+      router.goTo('a0/a1', onCreateRoute: _factory('a1'));
+      router.goTo('a0/a1/a2', onCreateRoute: _factory('a2'));
+      router.goTo('a0/a1/a2/a3', onCreateRoute: _factory('a3'));
+      expect(_nodesString(router.nodes), {'a0': { 'a1': { 'a2': { 'a3': {}}}}, 'b0': {}}.toString());
+
+      result = router.detach('a0/a1', 'c0');
+      expect(_fragmentsListString(result.stack), ['c0', 'a2', 'a3'].toString());
+      expect(_nodesString(router.nodes),
+          {'a0': {},
+           'b0': {},
+           'c0': { 'a2': { 'a3': {}}}}.toString());
+
+      router.goTo('a0', onCreateRoute: _errorFactory);
+      expect(_nodesString(router.nodes),
+          {'a0': {},
+           'b0': {},
+           'c0': { 'a2': { 'a3': {}}}}.toString());
+
+      result = router.detach('a0', 'd0');
+      expect(_fragmentsListString(result.stack), ['d0'].toString());
+      expect(_nodesString(router.nodes),
+          {'b0': {},
+           'c0': { 'a2': { 'a3': {}}},
+           'd0': {}}.toString());
     });
   });
 }
